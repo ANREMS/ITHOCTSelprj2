291e56eb94ca146a717dbc0baa6b732facd469ef
Annadurai <choannadurai@gmail.com>
2024-03-16 08:40:09 +0000

    Initial commit

--- src/test/java/org/example/Class2.java
+++ src/test/java/org/example/Class2.java
@@ -1,10 +1,8 @@
-
 package org.example;
 
-public class Class1 {
+public class Class2 {
 
     public static void main(String[] args) {
-        System.out.println("GIT Class1");
+        System.out.println("GIT Class2");
     }
-
 }
